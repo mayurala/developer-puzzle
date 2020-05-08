781719853861c3f2069389ac82910e8d9d69b08a
Abdul Quadir <aquadir83@gmail.com>
2020-05-08 16:56:37 +0530

    Task-4: Caching API call

--- apps/stocks-api/src/environments/environment.prod.ts
+++ apps/stocks-api/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'https://cloud.iexapis.com/beta/stock/'
 };
--- apps/stocks-api/src/main.ts
+++ apps/stocks-api/src/main.ts
@@ -3,20 +3,50 @@
  * This is only a minimal backend to get started.
  **/
 import { Server } from 'hapi';
+import { environment } from './environments/environment';
+
+const request = require('request');
 
 const init = async () => {
   const server = new Server({
     port: 3333,
-    host: 'localhost'
+    host: 'localhost',
+  });
+
+  const fetchStockQuote = async (symbol, period, token) => {
+
+    const stockURL = environment.apiURL + symbol + '/chart/' + period + '?token=' + token;
+    return new Promise((resolve, reject) => {
+      request(stockURL, (error, response, body) => {
+        if (error) reject(error);
+        if (response && response['statusCode'] === 200) {
+          resolve(body)
+        }
+        reject("Invalid status code : " + response.statusCode);
+
+      });
+    });
+
+  };
+
+  server.method('getStockQuote', fetchStockQuote, {
+    cache: {
+      expiresIn: 50 * 1000,
+      generateTimeout: 2000,
+    }
   });
 
   server.route({
     method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      return {
-        hello: 'world'
-      };
+    path: '/beta/stock/{symbol}/chart/{period}',
+    handler: async (request, h) => {
+
+      const { symbol, period } = request.params;
+      const token = request.query.token;
+
+      const result = await server.methods.getStockQuote(symbol, period, token);
+      return result;
+
     }
   });
 
