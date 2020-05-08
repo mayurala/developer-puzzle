06d33cdd544491c32c507c2dc5fafa0112e2d41d
Abdul Quadir <aquadir83@gmail.com>
2020-05-08 08:05:11 +0530

    Task-3: Choose custom dates to view trends

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
 
-interface TimePeriods {
-  viewValue: string,
-  value: string,
-}
+
 
 @Component({
   selector: 'coding-challenge-stocks',
@@ -14,33 +11,56 @@ interface TimePeriods {
 })
 export class StocksComponent implements OnInit {
   stockPickerForm: FormGroup;
+  maxDate: Date = new Date();
 
   quotes$ = this.priceQuery.priceQueries$;
 
-  timePeriods: TimePeriods[] = [
-    { viewValue: 'All available data', value: 'max' },
-    { viewValue: 'Five years', value: '5y' },
-    { viewValue: 'Two years', value: '2y' },
-    { viewValue: 'One year', value: '1y' },
-    { viewValue: 'Year-to-date', value: 'ytd' },
-    { viewValue: 'Six months', value: '6m' },
-    { viewValue: 'Three months', value: '3m' },
-    { viewValue: 'One month', value: '1m' }
-  ];
 
-  constructor(private fb: FormBuilder, private priceQuery: PriceQueryFacade) {
-    this.stockPickerForm = fb.group({
+  constructor(private fb: FormBuilder, private priceQuery: PriceQueryFacade) { }
+
+  ngOnInit() {
+    this.stockPickerForm = this.fb.group({
       symbol: ['', Validators.required],
-      period: ['', Validators.required]
+      fromDate: ['', Validators.required],
+      toDate: ['', Validators.required],
     });
+    //The date-pickers should not allow selection of dates after the current day.
+    this.stockPickerForm.valueChanges.subscribe(formValue => {
+      const { fromDate, toDate } = formValue;
+      if (toDate < fromDate) {
+        this.stockPickerForm.get('fromDate').setValue(toDate);
+      }
+    })
   }
-
-  ngOnInit() { }
 
   fetchQuote() {
     if (this.stockPickerForm.valid) {
-      const { symbol, period } = this.stockPickerForm.value;
+      const { symbol, fromDate, toDate } = this.stockPickerForm.value;
+      const timeDiff = Math.abs(fromDate - toDate);
+      const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+      const period = this.findPeriodByDays(diffDays); //find period based on custom dates;
       this.priceQuery.fetchQuote(symbol, period);
+    }
+  }
+
+  findPeriodByDays(days: number) {
+    if (days <= 30) {
+      return '1m';
+    } else if (days > 30 && days <= 91) {
+      return '3m';
+    }
+    else if (days > 91 && days <= 182) {
+      return '6m';
+    }
+    else if (days > 182 && days <= 365) {
+      return '1y';
+    } else if (days > 365 && days <= 730) {
+      return '2y';
+    } else if (days > 1095 && days <= 1095) {
+      return '3y';
+    }
+    else {
+      return 'max';
     }
   }
 }
